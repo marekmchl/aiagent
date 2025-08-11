555c1013a6fd9780eb68524b5f09f1f8150349e6
marekmchl <marekmchl@proton.me>
2025-08-11 07:56:10 +0200

    Read command line argument, write AI's response to the terminal

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    else:
+        res = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=sys.argv[1],
+        )
+    if res is not None:
+        if res.usage_metadata is not None:
+            print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
+        print(res.text)
 
 
 if __name__ == "__main__":
