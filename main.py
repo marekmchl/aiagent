1ebe5115e097b5861fe3d066dc0c3fe4ab3d9f0b
marekmchl <marekmchl@proton.me>
2025-08-12 08:58:27 +0200

    Add the --verbose flag and its functionality

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ def main():
     client = genai.Client(api_key=api_key)
     if len(sys.argv) < 2:
         sys.exit(1)
-    else:
-        messages = [
-            types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
-        ]
-        res = client.models.generate_content(
-            model="gemini-2.0-flash-001", contents=messages
-        )
+    verbose = False
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        verbose = True
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+    res = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
+    )
     if res is not None:
-        if res.usage_metadata is not None:
+        if res.usage_metadata is not None and verbose:
+            print(f"User prompt: {sys.argv[1]}")
             print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
         print(res.text)
