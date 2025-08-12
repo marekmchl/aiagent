91caf99576f0e530868f6de77d8ba0b6ccb0d874
marekmchl <marekmchl@proton.me>
2025-08-12 09:04:12 +0200

    Refactor code for readability

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ from google.genai import types
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+
     client = genai.Client(api_key=api_key)
-    if len(sys.argv) < 2:
-        sys.exit(1)
-    verbose = False
-    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
-        verbose = True
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
-    ]
+
+    user_prompt = sys.argv[1] if len(sys.argv) >= 2 else sys.exit(1)
+    verbose = True if (len(sys.argv) >= 3 and sys.argv[2] == "--verbose") else False
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
     res = client.models.generate_content(
         model="gemini-2.0-flash-001", contents=messages
     )
     if res is not None:
         if res.usage_metadata is not None and verbose:
-            print(f"User prompt: {sys.argv[1]}")
+            print(f"User prompt: {user_prompt}")
             print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
         print(res.text)
