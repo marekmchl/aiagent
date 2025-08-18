e075c0d56b992a264e1e6f408285a7590c114bba
marekmchl <marekmchl@proton.me>
2025-08-18 07:20:22 +0200

    Add a system prompt

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ def main():
 
     user_prompt = sys.argv[1] if len(sys.argv) >= 2 else sys.exit(1)
     verbose = True if (len(sys.argv) >= 3 and sys.argv[2] == "--verbose") else False
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
     res = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     if res is not None:
         if res.usage_metadata is not None and verbose:
